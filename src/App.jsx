364514b49e2fb3bd0fb51cf129aa84bfe2c3e4d0
JoaoPedroQueirozRocha <joaopedroqueirozrocha2004@gmail.com>
2022-10-13 17:46:04 -0300

    Fundos adicionados

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,6 @@ class App extends Component{
     let descricao = document.getElementById("Descricao")
     let local = document.getElementById("Local")
     let exibicao = document.getElementById("exibicao")
-    let alerta = document.getElementById("Alerta")
     if(dificuldade === "Facil"){
       exibicao.style = "display:flex"
       nome.innerHTML = `Nome da missão: ${this.state.nomeMissao}`;
@@ -96,9 +95,9 @@ class App extends Component{
       local.style = "color:red";
       descricao.style = "color:red";
     }
-    if(dificuldade === "Dificil" && this.state.numeroJogadores < 3){
+    if(dificuldade === "Dificil" && this.state.numeroJogadores < 3 || dificuldade === "Dificil" && this.state.numeroJogadores > 6){
       alert("Número de jogadores menor do que o recomendado, indicado de 3 a 6 jogadores")
-      nome.innerHTML = `Nome da missão ${this.state.nomeMissao}`;
+      nome.innerHTML = `Nome da missão: ${this.state.nomeMissao}`;
       numero.innerHTML = `Grupo: ${this.state.numeroJogadores}`;
       dificuldadeE.innerHTML = `Dificuldade: ${this.state.dificuldade}`;
       local.innerHTML = `Local: ${this.state.local}`
@@ -109,22 +108,9 @@ class App extends Component{
       local.style = "color:red";
       descricao.style = "color:red";
     }
-    else if(dificuldade === "Dificil" && this.state.numeroJogadores > 6){
-      alert("Número de jogadores menor do que o recomendado, indicado de 3 a 6 jogadores")
-      nome.innerHTML = `Nome da missão ${this.state.nomeMissao}`;
-      numero.innerHTML = `Grupo: ${this.state.numeroJogadores}`;
-      dificuldadeE.innerHTML = `Dificuldade: ${this.state.dificuldade}`;
-      local.innerHTML = `Local: ${this.state.local}`
-      descricao.innerHTML = this.state.descricaoMissao
-      nome.style = "color:red";
-      numero.style = "color:red";
-      dificuldadeE.style = "color:red";
-      local.style = "color:red";
-      descricao.style = "color:red";
-    }
-    if(dificuldade === "Medio" && this.state.numeroJogadores > 4){
+    if(dificuldade === "Medio" && this.state.numeroJogadores > 4 || dificuldade === "Medio" && this.state.numeroJogadores < 2){
       alert("Número de jogadores maior do que o recomendado, indicado de 2 a 4 jogadores")
-      nome.innerHTML = `Nome da missão ${this.state.nomeMissao}`;
+      nome.innerHTML = `Nome da missão: ${this.state.nomeMissao}`;
       numero.innerHTML = `Grupo: ${this.state.numeroJogadores}`;
       dificuldadeE.innerHTML = `Dificuldade: ${this.state.dificuldade}`;
       local.innerHTML = `Local: ${this.state.local}`
@@ -135,22 +121,9 @@ class App extends Component{
       local.style = "color:blue";
       descricao.style = "color:blue";
     }
-    else if(dificuldade === "Medio" && this.state.numeroJogadores < 2){
+    if(dificuldade === "Facil" && this.state.numeroJogadores < 2 || dificuldade === "Facil" && this.state.numeroJogadores > 4){
       alert("Número de jogadores menor do que o recomendado, indicado de 2 a 4 jogadores")
-      nome.innerHTML = `Nome da missão ${this.state.nomeMissao}`;
-      numero.innerHTML = `Grupo: ${this.state.numeroJogadores}`;
-      dificuldadeE.innerHTML = `Dificuldade: ${this.state.dificuldade}`;
-      local.innerHTML = `Local: ${this.state.local}`
-      descricao.innerHTML = this.state.descricaoMissao
-      nome.style = "color:blue";
-      numero.style = "color:blue";
-      dificuldadeE.style = "color:blue";
-      local.style = "color:blue";
-      descricao.style = "color:blue";
-    }
-    if(dificuldade === "Facil" && this.state.numeroJogadores < 2){
-      alert("Número de jogadores menor do que o recomendado, indicado de 2 a 4 jogadores")
-      nome.innerHTML = `Nome da missão ${this.state.nomeMissao}`;
+      nome.innerHTML = `Nome da missão: ${this.state.nomeMissao}`;
       numero.innerHTML = `Grupo: ${this.state.numeroJogadores}`;
       dificuldadeE.innerHTML = `Dificuldade: ${this.state.dificuldade}`;
       local.innerHTML = `Local: ${this.state.local}`
@@ -166,31 +139,35 @@ class App extends Component{
   render(){
     return(
       <div className='container'>
-        <label htmlFor="">Nome da missão:</label>
-        <input type="text" value={this.state.nomeMissao} onChange={this.armazenarNome}/>
-        <label htmlFor="">Número de jogadores</label>
-        <input type="number"  value={this.state.numeroJogadores} onChange={this.armazenarNumeros}/>
-        <label htmlFor="">Local da missão</label>
-        <input type="text" value={this.state.local} onChange={this.armazenarLocal} />
-        <label htmlFor="Descrição da missão"></label>
-        <textarea cols="30" rows="10" value={this.state.descricaoMissao} onChange={this.armazenarDescricao}></textarea>
-        <label htmlFor="">Dificuldade da missão</label>
-        <select name="" id="" value={this.state.dificuldade} onChange={this.armazenarDificuldade}>
-          <option></option>
-          <option value="Facil">Fácil</option>
-          <option value="Medio">Médio</option>
-          <option value="Dificil">Difícil</option>
-        </select>
-        <input type="submit" onClick={this.submit} />
-        <div id='exibicao'>
-          <p id='Alerta'></p>
-          <p id='Nome' className='resumo'></p>
-          <p id='Dificuldade' className='resumo'></p>
-          <p id='Numero' className='resumo'></p>
-          <p id='Local'></p>
-          <p id='Descricao' className='resumo'></p>
+        
+          <div className='containerMissao'>
+            <label htmlFor="">Nome da missão:</label>
+            <input type="text" value={this.state.nomeMissao} onChange={this.armazenarNome} />
+            <label htmlFor="">Número de aventureiros</label>
+            <input type="number"  value={this.state.numeroJogadores} onChange={this.armazenarNumeros} />
+            <label htmlFor="">Local da missão</label>
+            <input type="text" value={this.state.local} onChange={this.armazenarLocal} />
+            <label htmlFor="Descrição da missão">Descrição da missão</label>
+            <textarea cols="5" rows="7" value={this.state.descricaoMissao} onChange={this.armazenarDescricao} ></textarea>
+            <label htmlFor="">Dificuldade da missão</label>
+            <select name="" id="" value={this.state.dificuldade} onChange={this.armazenarDificuldade} >
+              <option></option>
+              <option value="Facil">Fácil</option>
+              <option value="Medio">Médio</option>
+              <option value="Dificil">Difícil</option>
+            </select>
+            <input type="submit" onClick={this.submit} id="btnSubmit"/>
+          </div>
+          <div id='exibicao'>
+            <p id='Nome'></p>
+            <p id='Dificuldade'></p>
+            <p id='Numero'></p>
+            <p id='Local'></p>
+            <p id='Descricao'></p>
+          </div>
         </div>
-      </div>
+
+
     )
   }
 }
